47f848680b25c8690ff1711bb26bdbc990cdfbbd
Emma Boardman <emma@emmaboardman.co.uk>
2016-10-05 11:52:48 +0100

    added restaurant api with objects in array

---
+++ client/src/models/restaurantAPI.js
@@ -0,0 +1,15 @@
+var RestaurantApi = function(app){
+  var restaurant = [
+  { name: "Khushi's",
+    cuisine: "Indian",
+    stars: 5
+    },
+
+    { name: "McDonalds",
+      cuisine: "Fast Food",
+      stars: 2
+      }
+
+
+  ]
+}
